ef505db11a0197c97db6f71bbf4dc93a2cd3c20f
ajnasajnas1234 <mubashiraajnas@gmail.com>
2023-09-30 22:50:26 +0530

    requirements

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -126,3 +126,5 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+ALLOWED_HOSTS = ['hospital_administration.onrender.com','127.0.0.1']

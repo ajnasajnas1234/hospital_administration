3a70a9fc672baa9f2ab50582d66834204e7c1603
ajnasajnas1234 <mubashiraajnas@gmail.com>
2023-09-30 22:45:55 +0530

    requirements

--- management/models.py
+++ management/models.py
@@ -9,7 +9,7 @@ from django.db import models
 
 class Booking(models.Model):
     p_name = models.CharField(max_length=500)
-    p_phone = models.IntegerField(max_length=10)
+    p_phone = models.IntegerField()
     p_email = models.EmailField()
     doc_name = models.CharField(max_length=500)
     booking_date = models.DateField()
